0927f3db2890a1efeb895700d023a1c04a7870e0
Vision <wuranwan2025@126.com>
2026-03-31 03:05:34 +0800

    Update

--- train.py
+++ train.py
@@ -42,7 +42,7 @@ def train(epoch, model, dataloader, optimizer, training):
 
 if __name__ == '__main__':
     # Arguments parsing
-    parser = argparse.ArgumentParser(description='Cross-Domain Few-Shot Semantic Segmentation Pytorch Implementation')
+    parser = argparse.ArgumentParser(description='Unleashing Generalization Potential of VFM for Cross-Domain Few-Shot Segmentation')
     parser.add_argument('--datapath', type=str, default='./Dataset')
     parser.add_argument('--benchmark', type=str, default='pascal')
     parser.add_argument('--backbone', type=str, choices=['resnet50', 'resnet101'], default='resnet50', help='backbone of semantic segmentation model')   
